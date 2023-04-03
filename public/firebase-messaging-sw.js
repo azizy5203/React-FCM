cc059dafc35e6ea5a3e94c37a2a970e0354ba27c
Youssef Abdulaziz <azizy5203@gmail.com>
2023-04-03 03:04:33 +0200

    sw to public

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,8 +1,23 @@
+console.log("sw")
+importScripts('https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasejs/9.19.1/firebase-messaging.js')
 // import { onMessage } from "firebase/messaging";
 // import { onBackgroundMessage } from "firebase/messaging/sw";
-// import { firebaseApp./initFirebaseebase";
+// import { firebaseApp } from "./initFirebaseebase";
 
-// const messaging = firebaseApp.messaging(firebaseApp.app);
+const firebaseConfig = {
+    apiKey: "AIzaSyDa5EOVViSnkuWksX8vbaLppudmbNNHcNk",
+    authDomain: "fir-23024.firebaseapp.com",
+    databaseURL:
+      "https://fir-23024-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "fir-23024",
+    storageBucket: "fir-23024.appspot.com",
+    messagingSenderId: "249974542444",
+    appId: "1:249974542444:web:a0fa84447ee465774bc849",
+  };
+
+const app = firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging(app);
 
 // // Handle incoming messages. Called when:
 // // - a message is received while the app has focus
@@ -14,17 +29,17 @@
 //   console.log("Message received. ", payload);
 // });
 
-// onBackgroundMessage(messaging, (payload) => {
-//   console.log(
-//     "[firebase-messaging-sw.js] Received background message ",
-//     payload
-//   );
-//   // Customize notification here
-//   const notificationTitle = "Background Message Title";
-//   const notificationOptions = {
-//     body: "Background Message body.",
-//     icon: "./assets/react.svg",
-//   };
-
-//   self.registration.showNotification(notificationTitle, notificationOptions);
-// });
+messaging.onBackgroundMessage(messaging, (payload) => {
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    payload
+  );
+  // Customize notification here
+  const notificationTitle = "Background Message Title";
+  const notificationOptions = {
+    body: "Background Message body.",
+    icon: "./assets/react.svg",
+  };
+    
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
